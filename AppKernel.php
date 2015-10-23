c68148a188a7ea46df62fb5a2f8fd02dc44271cf
julien.manganne <julien.manganne@extreme-sensio.com>
2015-10-23 09:18:28 +0200

    Added Monolog Bundle

--- AppKernel.php
+++ AppKernel.php
@@ -11,6 +11,7 @@ class AppKernel extends Kernel
         $bundles = array(
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \AppBundle\AppBundle(),
         );
